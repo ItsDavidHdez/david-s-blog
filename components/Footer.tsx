fac012edb3906b5aa95af8e39df04fe6d7319da9
David Vargas Hernandez <david-0610@live.com.mx>
2022-08-04 22:54:20 -0500

    change footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
 import styles from "../styles/Home.module.scss";
 
 const Footer: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.footer}>
-        <p>
-          David Vargas Hernández @ItsDavidHdez • Todos los izquierdos reservados{" "}
-          {new Date().getFullYear()}
+        <p style={{ display: "flex" }}>
+          Hecho con ❤️ por{" "}
+          <Link href="https://github.com/ItsDavidHdez">
+            <a>&nbsp;@ItsDavidHdez</a>
+          </Link>{" "}
+          • {new Date().getFullYear()}
         </p>
       </div>
     </div>
