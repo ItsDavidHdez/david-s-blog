3e84e3d92746c66763f619161f943c1ad3b542a8
David Vargas Hernandez <david-0610@live.com.mx>
2022-08-07 15:53:30 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,42 @@
 import Layout from "../components/Layout";
 import "../styles/globals.scss";
 import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <>
-        <Head>
-          <title>David Vargas Hernández</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta
-            name="description"
-            content="El blog de David Vargas Hernández"
-          />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    </Layout>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JFFKMG104P"
+      />
+      {/* eslint-disable-next-line @next/next/inline-script-id */}
+      <Script strategy="lazyOnload">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-JFFKMG104P');
+    `}
+      </Script>
+      <Layout>
+        <>
+          <Head>
+            <title>David Vargas Hernández</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <meta
+              name="description"
+              content="El blog de David Vargas Hernández"
+            />
+          </Head>
+          <Component {...pageProps} />
+        </>
+      </Layout>
+    </>
   );
 }
 
